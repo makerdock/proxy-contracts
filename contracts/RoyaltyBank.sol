d8ff722554ff3af9575d6a35f6e1994d909b4dbb
Abhishek Kumar <abhishekkumar35962@gmail.com>
2024-05-10 18:56:07 +0530

    fix: minor polishing done

---
+++ contracts/RoyaltyBank.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
+
+contract RoyaltyBank {
+    mapping(uint256 => uint256) public royalties;
+
+    function updateRewardsMapping(
+        uint256[] memory ids,
+        uint256[] memory rewards
+    ) public {
+        for (uint256 i = 0; i < ids.length; i++) {
+            royalties[ids[i]] = rewards[i];
+        }
+    }
+
+    function claimReward(uint256 id) public {
+        royalties[id] = 0;
+        payable(msg.sender).transfer(royalties[id]);
+    }
+}
