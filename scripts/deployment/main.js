b689963de8fcb28d1fdc320e00c9f1a80175d196
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2024-05-10 17:45:53 +0530

    fix: deployed in local blockchain

--- scripts/deployment/main.js
+++ scripts/deployment/main.js
@@ -4,24 +4,13 @@
 // You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
 // will compile your contracts, add the Hardhat Runtime Environment's members to the
 // global scope, and execute the script.
-const { network, run } = require("hardhat")
-
-const { deployApiConsumer } = require("./deployApiConsumer")
-const { deployAutomationCounter } = require("./deployAutomationCounter")
-const { deployPriceConsumerV3 } = require("./deployPriceConsumerV3")
-const { deployRandomNumberConsumer } = require("./deployRandomNumberConsumer")
-const {
-    deployRandomNumberDirectFundingConsumer,
-} = require("./deployRandomNumberDirectFundingConsumer")
+const { run } = require("hardhat")
+const { deployCasterNFTContract } = require("./deployNFTContract")
 
 async function main() {
     await run("compile")
-    const chainId = network.config.chainId
-    await deployApiConsumer(chainId)
-    await deployAutomationCounter(chainId)
-    await deployPriceConsumerV3(chainId)
-    await deployRandomNumberConsumer(chainId)
-    await deployRandomNumberDirectFundingConsumer(chainId)
+
+    await deployCasterNFTContract()
 }
 
 // We recommend this pattern to be able to use async/await everywhere
