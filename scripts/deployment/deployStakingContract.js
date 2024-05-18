2c4e942b983d0a99872b679c1dad7734f6844d58
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2024-05-18 10:03:46 +0530

    fix: deploy verification

--- scripts/deployment/deployStakingContract.js
+++ scripts/deployment/deployStakingContract.js
@@ -1,6 +1,8 @@
-const { ethers, run } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 async function deployStakingContract() {
+    console.log(network)
+
     const stakingContract = await ethers.getContractFactory("StakeNFT")
     const contract = await stakingContract.deploy()
 
