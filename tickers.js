1a36a81ea04ec67293e4cce34713d977dba37765
Abhishek Kumar <abhishekkumar35962@gmail.com>
2024-06-08 11:01:11 +0530

    fix: done with formula for calculating the initial tick

--- tickers.js
+++ tickers.js
@@ -3,16 +3,18 @@ const { BigNumber } = require("ethers")
 const bn = require("bignumber.js")
 const JSBI = require("jsbi")
 
-const token0 = BigNumber.from("3000000000000000000") // Example token0 value (1 ETH in wei)
-const token1 = BigNumber.from("200000000000000000000") // Example token1 value (0.5 ETH in wei)
+const token0 = BigNumber.from("6000000000000000000") // Example token0 value (1 ETH in wei)
+const token1 = BigNumber.from("1000000000000000000000") // Example token1 value (0.5 ETH in wei)
 const sqrtP = new bn(token0.toString()).div(token1.toString()).sqrt()
 
 const Q96 = new bn(2).pow(96)
 
-const P = sqrtP.multipliedBy(Q96).integerValue(3).toFixed(0)
+const P = sqrtP.multipliedBy(Q96).toFixed(0)
 
 console.log({ P })
 
 const result = TickMath.getTickAtSqrtRatio(JSBI.BigInt(P))
 
-console.log({ result })
+let adjustment = 200 * Math.floor(result / 200)
+
+console.log({ result, adjustment })
